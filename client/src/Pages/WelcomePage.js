13c1b1a412d4d5606f1b2b136551c080ac5a05aa
constantinadrian <adrianc.chiriac@yahoo.com>
2022-05-18 17:51:48 +0100

    [Add] Added vanta on main page

--- client/src/Pages/WelcomePage.js
+++ client/src/Pages/WelcomePage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import classes from './WelcomePage.module.css';
 import { Link } from 'react-router-dom';
+import useVanta from "../hooks/useVanta";
 
 const WelcomePage = (props) => {
 
+    const myRefDiv = useVanta();
+
     return (
-        <div className={`${classes["full-screen"]}`} >
+        <div className={`${classes["full-screen"]}`} ref={myRefDiv} >
             <div className={`d-flex justify-content-center align-items-center flex-column h-100 text-center text-break`}>
                 <h1>Weather App</h1>
                 <Link className="text-black" to="/weather">Enter</Link>
