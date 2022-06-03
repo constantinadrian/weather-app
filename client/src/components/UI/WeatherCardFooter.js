3a0411c801dfcb5584764e8f3b164af07791da09
constantinadrian <adrianc.chiriac@yahoo.com>
2022-06-03 22:44:24 +0100

    [Add] Added svg animation on weather card footer

--- client/src/components/UI/WeatherCardFooter.js
+++ client/src/components/UI/WeatherCardFooter.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import classes from "./WeatherCardFooter.module.css";
 
 const WeatherCardFooter = () => {
     return (
         <>
-            <Card.Footer className="text-muted">
+            <Card.Footer className="text-muted border-top-0">
                 Powered by{" "}
                 <a
                     href="https://www.weatherapi.com/"
@@ -15,6 +16,20 @@ const WeatherCardFooter = () => {
                     WeatherAPI.com
                 </a>
             </Card.Footer>
+            <footer className={`${classes['page-footer']}`}>
+                <div className={`${classes['footer-moon']}`}></div>
+                <div className={`${classes['footer-clouds']}`} ></div>
+                <hr className="mt-0"/>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col">
+                            <div className="footer-copyright position-relative text-center pb-3">
+                                © AC WEATHER 2022
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </>
     );
 };
